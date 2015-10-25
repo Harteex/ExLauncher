45ff6ef047bc4a3a426b11a6b3101023b99bc7eb
Harteex <harteex@gmail.com>
2015-10-25 23:47:02 +0100

    Issue #9 Initial support for reading OPKs Support for launching OPKs is still to be implemented

--- ExLauncher/AppManager.cpp
+++ ExLauncher/AppManager.cpp
@@ -40,6 +40,72 @@ void AppManager::AddApp(App* app)
 	}
 }
 
+#ifdef HAS_LIBOPK
+bool ListOpks(vector<string>& listToFill, string path)
+{
+	DIR* dir;
+	struct dirent* dnt;
+
+	dir = opendir(path.c_str());
+	if (dir == NULL)
+		return false;
+
+	while (dnt = readdir(dir))
+	{
+		string fileName = dnt->d_name;
+
+		if (fileName.length() < 5)
+			continue;
+
+		if (fileName.substr(fileName.length() - 4) != ".opk")
+			continue;
+
+		string fullPath = path + fileName;
+		listToFill.push_back(fullPath);
+	}
+
+	closedir(dir);
+
+	return true;
+}
+
+App* ParseOpkMetadata(struct OPK *opk)
+{
+	const char *key, *val;
+	size_t keyLength, valLength;
+	int ret;
+
+	App* app = new App();
+
+	while ((ret = opk_read_pair(opk, &key, &keyLength, &val, &valLength)))
+	{
+		if (ret < 0)
+		{
+			// TODO error that we could not read the metadata
+			delete app;
+			return NULL;
+		}
+
+		// Buffer value so that we get a null terminated string
+		char valueBuffer[valLength + 1];
+		sprintf(valueBuffer, "%.*s", valLength, val);
+
+		if (strncmp(key, "Categories", keyLength) == 0)
+		{
+			app->SetData("categories", valueBuffer);
+		}
+		else if (strncmp(key, "Name", keyLength) == 0)
+		{
+			app->SetData("name", valueBuffer);
+		}
+
+		// TODO handle more values, for example icon
+	}
+
+	return app;
+}
+#endif
+
 bool AppManager::LoadApps()
 {
 	// Create default categories (should always exist)
@@ -49,7 +115,62 @@ bool AppManager::LoadApps()
 	FindOrCreateCategory("emulators");
 
 #ifdef HAS_LIBOPK
-	// OPK CODE
+	
+	vector<string> opkPaths;
+	ListOpks(opkPaths, "/media/data/apps/");
+
+	for (string opkPath : opkPaths)
+	{
+		struct OPK *opk = opk_open(opkPath.c_str());
+		if (!opk)
+		{
+			// TODO error
+			continue;
+		}
+
+		while (true)
+		{
+			const char *metadataName;
+			int ret = opk_open_metadata(opk, &metadataName);
+			if (ret < 0)
+			{
+				// TODO error that loading metadata failed
+				break;
+			}
+			else if (!ret)
+			{
+				break;
+			}
+
+			string name(metadataName);
+			string platform = "";
+			size_t pos1 = string::npos;
+			size_t pos2 = string::npos;
+
+			pos2 = name.rfind(".");
+			if (pos2 != string::npos && pos2 > 0)
+			{
+				pos1 = name.rfind(".", pos2 - 1);
+				if (pos1 != string::npos)
+				{
+					pos1++;
+					platform = name.substr(pos1, pos2 - pos1);
+				}
+			}
+
+			// FIXME PLATFORM gcw0 is hardcoded
+			if (platform.compare("gcw0") == 0 || platform.compare("all") == 0)
+			{
+				// We have metadata to load
+				App* app = ParseOpkMetadata(opk);
+				if (app != NULL)
+					AddApp(app);
+			}
+		}
+
+		opk_close(opk);
+	}
+
 #else
 	// If no libopk, for example when compiling and testing with Visual Studio, just load example data
 
